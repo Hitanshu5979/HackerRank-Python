3241febd94f68bba91957ea4d88959570c74a75e
Hitanshu_5979 <53534136+Hitanshu5979@users.noreply.github.com>
2020-11-15 15:30:11 +0530

    Find the Runner-Up Score!

--- runner-up.py
+++ runner-up.py
@@ -1,8 +1,13 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    scores = list(arr)
+
+    #Converting Mapped Objects into List
+    scores = list(arr)  #Done in order to use list operations
+    
     high = max(scores)
+    
     while (high == max(scores)):
-        scores.remove(max(scores))
-    print(max(scores))
+        scores.remove(max(scores))  #Removes the highest score and it's duplicate entries
+    
+    print(max(scores))  #Prints Runner-Up Score
